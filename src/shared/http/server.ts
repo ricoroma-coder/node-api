ca8c048d06f847374d9cdbb5a999a3de4ebdaf31
Henrique Roma <roma.fulldev@gmail.com>
2021-08-26 18:14:27 -0300

    Criando rotas

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log("Inicio de tudo");
+import express from "express"
+import cors from "cors"
+import routes from "./routes"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => {
+	console.log("Server started on port 3000")
+})
