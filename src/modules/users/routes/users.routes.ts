57fbb2adf5f2cc69c98e6ada5f7ff7e3d0b6f9db
Henrique Roma <roma.fulldev@gmail.com>
2021-08-30 17:16:38 -0300

    Criando Users routes

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,57 @@
+import { Router } from 'express'
+import UsersController from '../controllers/UsersController'
+import { celebrate, Joi, Segments } from 'celebrate'
+
+const usersRouter = Router()
+const usersController = new UsersController()
+
+usersRouter.get('/', usersController.index)
+
+usersRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			email: Joi.string().email().required(),
+			password: Joi.string().required(),
+		}
+	}),
+	usersController.create
+)
+
+usersRouter.put(
+	'/:id',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			email: Joi.string().email().required(),
+			password: Joi.string().required(),
+		},
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	usersController.update
+)
+
+usersRouter.get(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	usersController.show
+)
+
+usersRouter.delete(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	usersController.delete
+)
+
+export default usersRouter
