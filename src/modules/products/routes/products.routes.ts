ff318a78b524b34cd081256d4eaffbf86b56c838
Henrique Roma <roma.fulldev@gmail.com>
2021-08-30 15:29:09 -0300

    Importando o 'celebrate' e utilizando para fazer validações dos dados enviados as rotas

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express'
 import ProductsController from '../controllers/ProductsController'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const productsRouter = Router()
 const productsController = new ProductsController()
 
 productsRouter.get('/', productsController.index)
-productsRouter.post('/', productsController.create)
-productsRouter.put('/:id', productsController.update)
-productsRouter.get('/:id', productsController.show)
-productsRouter.delete('/:id', productsController.delete)
+
+productsRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			price: Joi.number().precision(2).required(),
+			quantity: Joi.number().required()
+		}
+	}),
+	productsController.create
+)
+
+productsRouter.put(
+	'/:id',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			price: Joi.number().precision(2).required(),
+			quantity: Joi.number().required()
+		},
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	productsController.update
+)
+
+productsRouter.get(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	productsController.show
+)
+
+productsRouter.delete(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required()
+		}
+	}),
+	productsController.delete
+)
 
 export default productsRouter
