a97f2fb2bccfa2f33b262645adfc6a402b7e1cca
Henrique Roma <roma.fulldev@gmail.com>
2021-08-30 15:39:28 -0300

    Criando migration Users

---
+++ src/shared/typeorm/migrations/1630348295957-CreateUsers.ts
@@ -0,0 +1,26 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1630348295957 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'users',
+				columns: [
+					{name: 'id', type: 'uuid', isPrimary: true, generationStrategy: 'uuid', default: 'uuid_generate_v4()' },
+					{name: 'name', type: 'varchar' },
+					{name: 'email', type: 'varchar', isUnique: true },
+					{name: 'password', type: 'varchar' },
+					{name: 'avatar', type: 'varchar', isNullable: true },
+					{name: 'created_at', type: 'timestamp with time zone', default: 'now()'},
+					{name: 'updated_at', type: 'timestamp with time zone', default: 'now()'}
+				]
+			})
+		)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('users')
+    }
+
+}
