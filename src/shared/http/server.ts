b5c7f0d40da49eb4c9889b9dad6961529c7af40c
Henrique Roma <roma.fulldev@gmail.com>
2021-08-31 13:45:26 -0300

    Criando migration UserTokens

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,11 +6,13 @@ import routes from "./routes"
 import AppError from "@shared/errors/AppError"
 import '@shared//typeorm'
 import { errors } from 'celebrate'
+import upload from '@config/upload'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(upload.directory))
 app.use(routes)
 
 app.use(errors())
