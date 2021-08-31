a2ecbb9599d1f3d29e08037c9f48c86a5e9231df
Henrique Roma <roma.fulldev@gmail.com>
2021-08-31 10:43:12 -0300

    Criando middleware e sobrescrevendo type Request

--- src/modules/users/middlewares/isAuthenticated.ts
+++ src/modules/users/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from "@shared/errors/AppError";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+interface TokenPayload {
+	iat: number
+	exp: number
+	sub: string
+}
+
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
 	const authHeader = request.headers.authorization
 
@@ -13,6 +19,12 @@ export default function isAuthenticated(request: Request, response: Response, ne
 
 	try {
 		const decodedToken = verify(token, auth.jwt.secret)
+
+		const { sub } = decodedToken as TokenPayload
+
+		request.user = {
+			id: sub
+		}
 
 		return next()
 	} catch {
